c71b58790ef2240fd121e089656e1a680f2d5a6b
YuliaKravchukS <ljuljek554@gmail.com>
2024-01-02 15:16:31 +0200

    task-6 start

--- js/task-6.js
+++ js/task-6.js
@@ -4,4 +4,39 @@ function getRandomHexColor() {
     .padStart(6, 0)}`;
 }
 
-const btnCreate =document.querySelector()
+const btnCreate = document.querySelector('button[data-create]');
+const btnDestroy = document.querySelector('button[data-destroy]');
+const textInput = document.querySelector('#controls input')
+const boxes = document.querySelector('#boxes')
+
+
+btnCreate.addEventListener('click', createBoxes);
+btnDestroy.addEventListener('click', destroyBoxes)
+
+
+console.log(textInput.value);
+const amount = 3;
+const min = textInput.getAttribute('min');
+const max = textInput.getAttribute('max');
+console.log(amount, min, max);
+
+function createBoxes(amount) {
+  const arr = [];
+  let count = 0;
+    for (let i = 0; i < amount; i++) {
+      if (amount >= min && amount <= max) {
+        const markupBox = '<div class = box></div>';
+        arr.push(markupBox);
+      }
+  }
+  return arr
+}
+
+
+
+console.log(createBoxes());
+
+function destroyBoxes() {
+
+}
+
